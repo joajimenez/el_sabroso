56681e13006c50e933dd68d008e10ac5913ac89d
Joan Jimenez <jimenez.donaldson@gmail.com>
2023-09-06 20:10:27 +0000

    Completed the menu's drinks section

--- src/index.js
+++ src/index.js
@@ -1,107 +1,111 @@
-import './style.css';
-import '@picocss/pico/css/pico.min.css';
-import { createContentEl, createElement, createImage, createNav, createCard } from './utils.js';
-import { app } from './ui.js';
-import { createDrinkCardEl, tropicalDelights } from './menu.js'
-import menu from './menu_data.json'
-import heroPhoto from './rest_hero.jpg'
+import "./style.css";
+import "@picocss/pico/css/pico.min.css";
+import {
+  createContentEl,
+  createElement,
+  createImage,
+  createNav,
+  createCard,
+} from "./utils.js";
+import { app } from "./ui.js";
+import { createMenuContent, tropicalDelights } from "./menu.js";
+import heroPhoto from "./rest_hero.jpg";
 
-const contentEL = createContentEl()
-// app.appendChild(contentEL)
+export const contentEL = createContentEl();
 
-// console.log(menu.Drinks['Tropical Delights'])
+let delightsArray = Object.entries(tropicalDelights);
+console.log(delightsArray);
 
-const tropicalDelightTitles = Object.keys(menu.Drinks['Tropical Delights']);
-const tropicalDelightDescriptions = Object.values(menu.Drinks['Tropical Delights']);
-// console.log(drinksTitles);
+for (let array of delightsArray) {
+  console.log(`${array[0]}: ${array[1]}`);
+}
 
+// const tropicalDelightTitles = Object.keys(tropicalDelights);
+// const tropicalDelightDescriptions = Object.values(tropicalDelights);
 
-console.log(tropicalDelightTitles[0], tropicalDelightDescriptions[0]);
+// console.log(`${tropicalDelightTitles[0]}: ${tropicalDelightDescriptions[0]}`);
 
 const createNewNav = () => {
-  const navbar = createNav('El Sabroso', [
-    { text: 'MENU', href: '#', id: 'menuBtn' },
-    { text: 'ABOUT', href: '#', id: 'aboutBtn' },
-    { text: 'RESERVATIONS', href: '#', id: 'reservationsBtn' },
+  const navbar = createNav("El Sabroso", [
+    { text: "MENU", href: "#", id: "menuBtn" },
+    { text: "ABOUT", href: "#", id: "aboutBtn" },
+    { text: "RESERVATIONS", href: "#", id: "reservationsBtn" },
   ]);
 
   app.appendChild(navbar);
 };
 
-
 const createIndexContent = () => {
-  app.appendChild(contentEL)
+  app.appendChild(contentEL);
 
   const newHeroImage = new Image();
   newHeroImage.src = heroPhoto;
 
   contentEL.appendChild(newHeroImage);
 
-
   const descriptionContainerEl = createElement(
-    'div',
-    'descriptionContainerEl',
-    ''
+    "div",
+    "descriptionContainerEl",
+    ""
   );
 
   const introText = createElement(
-    'p',
-    'introText',
+    "p",
+    "introText",
     "Welcome to EL SABROSO, a restaurant that celebrates the delicious flavors of the Dominican Republic. We use only the freshest, highest-quality ingredients to create our authentic Dominican dishes, and our chefs are experts in traditional Dominican cuisine. We offer a wide variety of dishes, so there's something for everyone to enjoy."
   );
 
   const bodyParaOne = createElement(
-    'p',
-    'bodyParaOne',
+    "p",
+    "bodyParaOne",
     "Our passion for Dominican food is evident in every bite. We believe that food is more than just sustenance; it's a way to connect with our culture and heritage. That's why we take great care in sourcing our ingredients and preparing our dishes. We want you to experience the true flavors of the Dominican Republic, and we believe that our food is the best way to do that."
   );
 
   const bodyParaTwo = createElement(
-    'p',
-    'bodyParaTwo',
+    "p",
+    "bodyParaTwo",
     "We also believe in providing our customers with a warm and welcoming experience. Our staff is friendly and attentive, and they're always happy to help you choose the perfect dish. We want you to feel like you're part of our family when you dine with us."
   );
 
   const bodyParaThree = createElement(
-    'p',
-    'bodyParaThree',
+    "p",
+    "bodyParaThree",
     "We're proud to be a part of the Dominican community in our city, and we hope you'll join us for a meal soon!"
   );
 
   descriptionContainerEl.appendChild(introText);
-  descriptionContainerEl.appendChild(bodyParaOne)
+  descriptionContainerEl.appendChild(bodyParaOne);
   contentEL.appendChild(descriptionContainerEl);
 
-  const callToActionEl = createElement('div', 'callToActionEl', '');
+  const callToActionEl = createElement("div", "callToActionEl", "");
   const callToActionBtn = createElement(
-    'button',
-    'callToActionBtn',
-    'BOOK NOW'
+    "button",
+    "callToActionBtn",
+    "BOOK NOW"
   );
 
   contentEL.appendChild(callToActionEl);
   callToActionEl.appendChild(callToActionBtn);
-
 };
 
 function main() {
-
   createNewNav();
   createIndexContent();
 
-  const elSabrosoBtn = document.querySelector('li strong');
-  const menuBtn = document.getElementById('menuBtn');
-  const aboutBtn = document.getElementById('aboutBtn');
-  const reservationsBtn = document.getElementById('reservationsBtn');
+  const elSabrosoBtn = document.querySelector("li strong");
+  const menuBtn = document.getElementById("menuBtn");
+  const aboutBtn = document.getElementById("aboutBtn");
+  const reservationsBtn = document.getElementById("reservationsBtn");
 
-  elSabrosoBtn.addEventListener('click', () => {
-    contentEL.innerHTML = ''
+  elSabrosoBtn.addEventListener("click", () => {
+    contentEL.innerHTML = "";
     createIndexContent();
-  })
+  });
 
-  menuBtn.addEventListener('click', function() {
-    contentEL.innerHTML = ''
-    console.log(tropicalDelights)
+  menuBtn.addEventListener("click", function () {
+    contentEL.innerHTML = "";
+    console.log(tropicalDelights);
+    createMenuContent();
   });
 }
 
--- src/menu.js
+++ src/menu.js
@@ -1,68 +1,66 @@
-import { createElement, createImage, createCard } from './utils.js';
-import { app } from './ui.js';
-import  menu  from './menu_data.json';
+import { createElement, createImage, createCard } from "./utils.js";
+import { app } from "./ui.js";
+import { contentEL } from "./index.js";
+import menu from "./menu_data.json";
 
-export const tropicalDelights = menu.Drinks['Tropical Delights'];
+export const tropicalDelights = menu.Drinks["Tropical Delights"];
 
+export const createMenuContent = () => {
+  const drinksEl = createElement("div", "drinksEl", "");
 
+  drinksEl.appendChild(createElement("h2", "drinksTitle", "Drinks"));
+  drinksEl.appendChild(
+    createElement("p", "introText", menu.Drinks["Intro Text"])
+  );
 
-// const createMenuContent = () => {
-//   const drinksEl = createElement('div', 'drinksEl', '');
-//   const mainDishesEl = createElement('div', 'mainDishesEl', '');
-//   const dessertsEl = createElement('div', 'dessertsEl', '');
+  contentEL.appendChild(drinksEl);
 
-//   app.appendChild(drinksEl);
-//   drinksEl.appendChild(createElement('h2', 'drinksTitle', 'Drinks'));
-//   drinksEl.appendChild(createElement('p', 'introText', drinks.introText));
+  const drinksSectionOne = createElement("article", "drinksSectionOne", "");
+  const tropicalDelightsHeader = createElement(
+    "header",
+    "tropicalDelightsHeader",
+    "Tropical Delights"
+  );
+  const drinksSectionOneBody = createElement("div", "drinksSectionOneBody", "");
 
-//   app.appendChild(mainDishesEl);
-//   mainDishesEl.appendChild(
-//     createElement('h2', 'mainDishesTitle', 'Main Dishes')
-//   );
+  drinksSectionOne.appendChild(tropicalDelightsHeader);
+  drinksSectionOne.appendChild(drinksSectionOneBody);
 
-//   app.appendChild(dessertsEl);
-//   dessertsEl.appendChild(createElement('h2', 'dessertsTitle', 'Desserts'));
+  let delightsArray = Object.entries(tropicalDelights);
+  console.log(delightsArray);
 
-//   const drinksIntroText = createElement(
-//     'p',
-//     'drinksIntroText',
-//     `${drinks.introText}`
-//   );
+  for (let array of delightsArray) {
+    let title = createElement("h5", "card-title", `${array[0]}`);
+    let description = createElement("p", "card-description", `${array[1]}`);
 
-//   const drinksSectionOne = createElement('div', 'drinksSectionOne', '');
-//   const drinksSectionTwo = createElement('div', 'drinksSectionTwo', '');
+    drinksSectionOneBody.appendChild(title);
+    drinksSectionOneBody.appendChild(description);
+  }
 
-//   const bodyParaTwo = createElement(
-//     'p',
-//     'bodyParaTwo',
-//     "We also believe in providing our customers with a warm and welcoming experience. Our staff is friendly and attentive, and they're always happy to help you choose the perfect dish. We want you to feel like you're part of our family when you dine with us."
-//   );
+  const mainDishes = createElement("div", "drinksSectionTwo", "");
 
-//   const bodyParaThree = createElement(
-//     'p',
-//     'bodyParaThree',
-//     "We're proud to be a part of the Dominican community in our city, and we hope you'll join us for a meal soon!"
-//   );
+  drinksEl.appendChild(drinksSectionOne);
 
-//   const bodyCopyEl = createElement('div', 'bodyCopy', '');
+  //   const mainDishesEl = createElement('div', 'mainDishesEl', '');
+  //   const dessertsEl = createElement('div', 'dessertsEl', '');
 
-//   // app.appendChild(siteTitle);
+  //   app.appendChild(mainDishesEl);
+  //   mainDishesEl.appendChild(
+  //     createElement('h2', 'mainDishesTitle', 'Main Dishes')
+  //   );
 
-//   app.appendChild(descriptionContainerEl);
-//   descriptionContainerEl.appendChild(introText);
+  //   app.appendChild(dessertsEl);
+  //   dessertsEl.appendChild(createElement('h2', 'dessertsTitle', 'Desserts'));
 
-//   app.appendChild(bodyCopyEl);
-//   // bodyCopy.appendChild(bodyParaOne);
-//   // bodyCopy.appendChild(bodyParaTwo);
-//   bodyCopyEl.appendChild(bodyParaThree);
+  // app.appendChild(siteTitle);
 
-//   const callToActionEl = createElement('div', 'callToActionEl', '');
-//   const callToActionBtn = createElement(
-//     'button',
-//     'callToActionBtn',
-//     'BOOK NOW'
-//   );
+  const callToActionEl = createElement("div", "callToActionEl", "");
+  const callToActionBtn = createElement(
+    "button",
+    "callToActionBtn",
+    "BOOK NOW"
+  );
 
-//   app.appendChild(callToActionEl);
-//   callToActionEl.appendChild(callToActionBtn);
-// };
+  app.appendChild(callToActionEl);
+  callToActionEl.appendChild(callToActionBtn);
+};
