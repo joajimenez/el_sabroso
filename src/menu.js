dc6b7a01b886da46a4f895713b3f4126d84cd480
Joan Jimenez <jimenez.donaldson@gmail.com>
2023-09-06 20:21:13 +0000

    Added Main Dishes section.

--- src/menu.js
+++ src/menu.js
@@ -4,16 +4,15 @@ import { contentEL } from "./index.js";
 import menu from "./menu_data.json";
 
 export const tropicalDelights = menu.Drinks["Tropical Delights"];
+export const mainDishes = menu["Main Dishes"].Tradicionales;
 
-export const createMenuContent = () => {
+function creacteDrinksSection() {
   const drinksEl = createElement("div", "drinksEl", "");
 
   drinksEl.appendChild(createElement("h2", "drinksTitle", "Drinks"));
   drinksEl.appendChild(
     createElement("p", "introText", menu.Drinks["Intro Text"])
   );
-
-  contentEL.appendChild(drinksEl);
 
   const drinksSectionOne = createElement("article", "drinksSectionOne", "");
   const tropicalDelightsHeader = createElement(
@@ -37,9 +36,61 @@ export const createMenuContent = () => {
     drinksSectionOneBody.appendChild(description);
   }
 
-  const mainDishes = createElement("div", "drinksSectionTwo", "");
-
   drinksEl.appendChild(drinksSectionOne);
+
+  contentEL.appendChild(drinksEl);
+}
+
+function createMainDishesSection() {
+  const mainDishesEl = createElement("div", "mainDishesEl", "");
+
+  mainDishesEl.appendChild(
+    createElement("h2", "mainDishesTitle", "Main Dishes")
+  );
+  mainDishesEl.appendChild(
+    createElement("p", "introText", menu.Drinks["Intro Text"])
+  );
+
+  const mainDishesSectionOne = createElement(
+    "article",
+    "mainDishesSectionOne",
+    ""
+  );
+  const mainDishesHeader = createElement(
+    "header",
+    "mainDishesHeader",
+    "Tradicionales"
+  );
+  const mainDishesSectionOneOneBody = createElement(
+    "div",
+    "mainDishesSectionOneBody",
+    ""
+  );
+
+  mainDishesSectionOne.appendChild(mainDishesHeader);
+  mainDishesSectionOne.appendChild(mainDishesSectionOneOneBody);
+
+  let mainDishesArray = Object.entries(mainDishes);
+  console.log(mainDishesArray);
+
+  for (let array of mainDishesArray) {
+    let title = createElement("h5", "card-title", `${array[0]}`);
+    let description = createElement("p", "card-description", `${array[1]}`);
+
+    mainDishesSectionOneOneBody.appendChild(title);
+    mainDishesSectionOneOneBody.appendChild(description);
+  }
+
+  mainDishesEl.appendChild(mainDishesSectionOne);
+
+  contentEL.appendChild(mainDishesEl);
+}
+
+export const createMenuContent = () => {
+  creacteDrinksSection();
+  createMainDishesSection();
+
+  // const mainDishes = createElement("div", "drinksSectionTwo", "");
 
   //   const mainDishesEl = createElement('div', 'mainDishesEl', '');
   //   const dessertsEl = createElement('div', 'dessertsEl', '');
